9c6163339a6036cd6159ef5439ed60f9c40497b6
Sikander Ktk <sikanderktk222@gmail.com>
2025-10-18 17:30:00 +0500

    Refactor app.py for heart disease prediction app

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ model = joblib.load(MODEL_PATH)
 # -----------------------------
 # Images
 # -----------------------------
-heart_img_path = BASE_DIR / "heartpic.png"
+heart_img_path = "heartpic.png"
 predict_img_path = BASE_DIR / "heartpics.png"
 
 # -----------------------------
@@ -177,3 +177,4 @@ It demonstrates **data preprocessing, model training, and deployment using Strea
 # Footer
 # -----------------------------
 st.markdown("<p class='footer'>👨‍💻 Developed by Sikander Ktk | Made with ❤️ using Streamlit & Scikit-learn</p>", unsafe_allow_html=True)
+
